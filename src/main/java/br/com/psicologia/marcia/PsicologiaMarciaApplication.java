60ec3a1a2a4ab2dcdc73cd690d28e720e3a8b7ff
Bruno Fernandes <brunus.desenvolvimentobackfront@gmail.com>
2024-08-24 11:43:34 -0300

    realizado configurações iniciais do projeto

---
+++ src/main/java/br/com/psicologia/marcia/PsicologiaMarciaApplication.java
@@ -0,0 +1,14 @@
+package br.com.psicologia.marcia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsicologiaMarciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsicologiaMarciaApplication.class, args);
+		System.out.println("Sistema de psicologia-marcia-regina-rodando !!!");
+	}
+
+}
